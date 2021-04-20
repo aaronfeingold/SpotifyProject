ccbcd4f474b2a22de730e1b22e362118307b157d
aaronfeingold <ajfeingold88@gmail.com>
2021-04-20 13:43:42 -0500

    send_sms method loops

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import json
 
 secrets = json.loads(open("secrets.json").read())
 
-user_names = ['22gnzesxvy4aaxsab3fbxkq6y', 'a.dglazier', '1214200651']
+dev_user_names = ['22gnzesxvy4aaxsab3fbxkq6y', 'a.dglazier', '1214200651']
+dev_numbers = ['+12017879112']
 
 sa = SpotifyAuthenticator(client_id=secrets["SPOTIFY_CLIENT_ID"], client_secret=secrets["SPOTIFY_CLIENT_SECRET"])
 song_getter = SongGetter(sp=sa.sp)
-songs = [song_getter.get_song(name) for name in user_names]
-text_sender = SendTextMessage(songs=songs, account_sid=secrets["TWILIO_ACCT_SID"], auth_token=secrets["TWILIO_AUTH_TOKEN"])
+songs = [song_getter.get_song(name) for name in dev_user_names]
+text_sender = SendTextMessage(songs=songs, numbers=dev_numbers, account_sid=secrets["TWILIO_ACCT_SID"], auth_token=secrets["TWILIO_AUTH_TOKEN"])
 text_sender.send_sms()
--- services/texter.py
+++ services/texter.py
@@ -2,17 +2,11 @@ from decouple import config
 from twilio.rest import Client
 
 class SendTextMessage:
-  def __init__(self, songs, account_sid, auth_token):
+  def __init__(self, songs, numbers, account_sid, auth_token):
     self.songs = songs
-    self.account_sid = account_sid
-    self.auth_token = auth_token
-    self.client = self.create_twilio_client()
+    self.numbers = numbers
+    self.client = Client(username=account_sid, password=auth_token)
     self.message = self.set_message()
-
-  def create_twilio_client(self):
-      new_client = Client(username=self.account_sid, password=self.auth_token)
-
-      return new_client
 
 
   def set_message(self):
@@ -32,11 +26,12 @@ class SendTextMessage:
 
   def send_sms(self):
     joined_sentances = "\n\n".join(self.message)
-    final_message = self.client.messages \
-                .create(
-                    body=joined_sentances,
-                    from_='+13478307901',
-                    to='+12017879112'
-                )
-    return final_message
+
+    for number in self.numbers:
+      final_message = self.client.messages \
+                  .create(
+                      body=joined_sentances,
+                      from_='+13478307901',
+                      to=number
+                  )
 
