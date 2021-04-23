f27a2cd99dd724f862c02541bc5e8be0b61e3836
aaronfeingold <ajfeingold88@gmail.com>
2021-04-23 16:49:56 -0500

    deploy test

--- main.py
+++ main.py
@@ -13,10 +13,14 @@ def run_main(event, context):
   tai = os.environ["TWILIO_ACCT_SID"]
   tat = os.environ["TWILIO_AUTH_TOKEN"]
   
+  dev_user_names = ["22gnzesxvy4aaxsab3fbxkq6y", "a.dglazier", "1214200651"]
+  dev_numbers = ["+12017879112"]
+  app_number = ["+13478307901"]
+  
   sa = SpotifyAuthenticator(client_id=sci, client_secret=scs)
   song_getter = SongGetter(sp=sa.sp)
-  songs = [song_getter.get_song(name) for name in data["dev_user_names"]]
+  songs = [song_getter.get_song(name) for name in dev_user_names]
   text_sender = SendTextMessage(account_sid=tai, auth_token=tat)
   message = text_sender.set_message(songs=songs)
-  text_sender.send_sms(app_number=data["app_number"], numbers=data["dev_numbers"], message=message)
+  text_sender.send_sms(app_number=app_number, numbers=dev_numbers, message=message)
 
