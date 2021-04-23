0021c6cabe06cfd2280b25a295da231cd221d95a
aaronfeingold <ajfeingold88@gmail.com>
2021-04-23 13:29:03 -0500

    reduced redundancies

--- services/texter.py
+++ services/texter.py
@@ -22,7 +22,6 @@ class SendTextMessage:
 
 
   def send_sms(self, app_number, numbers, message):
-    sids = []
     errors = []
 
     for number in numbers:
@@ -34,13 +33,11 @@ class SendTextMessage:
                   )
       if final_message.error_code:
         errors.append(final_message.error_message)
-      else:
-        sids.append(final_message.sid)
 
     if len(errors) != 0:
       return errors
     else:
-      return sids
+      print("send text completed, check phone")
 
 
   
