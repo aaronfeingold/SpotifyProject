b4e785680c6b7a7c04a06c8132b64321eadf3a25
aaronfeingold <ajfeingold88@gmail.com>
2021-04-23 15:05:29 -0500

    updated lambda handler function for AWS

--- main.py
+++ main.py
@@ -5,33 +5,21 @@ from dotenv import load_dotenv
 import json
 import os
 
-load_dotenv()
+def run_main(event, context):
+  load_dotenv()
 
-spotify_client_id = os.environ["SPOTIFY_CLIENT_ID"]
-spotify_client_secret = os.environ["SPOTIFY_CLIENT_SECRET"]
-twilio_acct_sid = os.environ["TWILIO_ACCT_SID"]
-twilio_auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+  sci = os.environ["SPOTIFY_CLIENT_ID"]
+  scs = os.environ["SPOTIFY_CLIENT_SECRET"]
+  tai = os.environ["TWILIO_ACCT_SID"]
+  tat = os.environ["TWILIO_AUTH_TOKEN"]
 
-with open('data.json') as json_file:
-  data = json.load(json_file)
-
-
-def lambda_handler(event, context):
-    run_main()
-
-
-def run_main():
-  sa = SpotifyAuthenticator(client_id=spotify_client_id, client_secret=spotify_client_secret)
+  with open('data.json') as json_file:
+    data = json.load(json_file)
+  
+  sa = SpotifyAuthenticator(client_id=sci, client_secret=scs)
   song_getter = SongGetter(sp=sa.sp)
   songs = [song_getter.get_song(name) for name in data["dev_user_names"]]
-  text_sender = SendTextMessage(account_sid=twilio_acct_sid, auth_token=twilio_auth_token)
+  text_sender = SendTextMessage(account_sid=tai, auth_token=tat)
   message = text_sender.set_message(songs=songs)
   text_sender.send_sms(app_number=data["app_number"], numbers=data["dev_numbers"], message=message)
 
-
-
-def test():
-  lambda_handler(event=data["dev_cloudwatch_trigger"], context=None)
-
-
-test()
